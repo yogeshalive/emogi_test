92af7791965ce0dddc408af0eacac34358e37816
yogeshalive <yogeshalive@gmail.com>
2017-08-02 21:12:28 +0530

    added cursor postion

--- emogi.js
+++ emogi.js
@@ -1,59 +1,73 @@
 (function (EMOGI) {
-   
+   let lastFocus = 0;
    const config = {
-      emogis : [{
+      emogis: [{
          sortCut: ':)',
          imgName: 'smile',
          spacialChar: '',
          uniCode: '',
       }, {
-         sortCut: '<3',
-         spacialChar: '<',
-         uniCode: '&lt;',
-         imgName: 'heart'
-      }]
-   },
-   imgRoot = './assets/';
-   placeCaretAtEnd = (el) => {
-      el.focus();
+            sortCut: '<3',
+            spacialChar: '<',
+            uniCode: '&lt;',
+            imgName: 'heart'
+         }]
+      },
+      imgRoot = './assets/';
+      
+   placeCaret = (el) => {
       if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
-            var range = document.createRange();
-            range.selectNodeContents(el);
-            range.collapse(false);
-            var sel = window.getSelection();
-            sel.removeAllRanges();
-            sel.addRange(range);
-      } else if (typeof document.body.createTextRange != "undefined") {
-            var textRange = document.body.createTextRange();
-            textRange.moveToElementText(el);
-            textRange.collapse(false);
-            textRange.select();
+         let range = document.createRange(),
+            operatedSelection = lastFocus,
+            operatedChildNode;
+         range.selectNodeContents(el);
+         for (let index = 0; index < el.childNodes.length; index++) {
+            const elmNode = el.childNodes[index];
+            if (elmNode.length > operatedSelection || (elmNode.localName === 'img' && operatedSelection === 0)) {
+               operatedChildNode = el.childNodes[index + 1];
+               break;
+            } else {
+               operatedSelection -= elmNode.length ? elmNode.length : elmNode.outerHTML.length;
+            }
+         }
+         range.setStart(operatedChildNode, operatedSelection);
+         range.collapse(true);
+         let sel = window.getSelection();
+         sel.removeAllRanges();
+         sel.addRange(range);
+
       }
+      el.focus();
    };
 
 
    EMOGI.changeHandler = (_this) => {
       let valueOfInput = _this.innerHTML,
          twoChar,
-         isShortCutFound = false;
+         isShortCutFound = false,
+         emogiName = '';
 
       config.emogis.forEach((emogi, index) => {
-            valueOfInput = valueOfInput.replace(new RegExp(emogi.uniCode, 'gi'), emogi.spacialChar);
-            if (valueOfInput.indexOf(emogi.sortCut) !== -1) {
-                  const indexOfSortCut = valueOfInput.indexOf(emogi.sortCut),
-                  lastIndexAfterShortCut = valueOfInput.substring(indexOfSortCut + emogi.sortCut.length , valueOfInput.length);;
-                  isShortCutFound = true;
-                  valueOfInput = valueOfInput.substring(0, indexOfSortCut);
+         valueOfInput = valueOfInput.replace(new RegExp(emogi.uniCode, 'gi'), emogi.spacialChar).replace(new RegExp('&nbsp;', 'gi'), ' ');
 
-                  valueOfInput+= '<img src="'+ imgRoot + emogi.imgName +'.png" width="20" height="20">';
-                  valueOfInput+= lastIndexAfterShortCut;
-            }
-            
+         if (valueOfInput.indexOf(emogi.sortCut) !== -1) {
+
+            const indexOfSortCut = valueOfInput.indexOf(emogi.sortCut),
+               lastIndexAfterShortCut = valueOfInput.substring(indexOfSortCut + emogi.sortCut.length, valueOfInput.length);;
+            isShortCutFound = true;
+            lastFocus = indexOfSortCut;
+            valueOfInput = valueOfInput.substring(0, indexOfSortCut);
+
+            valueOfInput += '<img src="' + imgRoot + emogi.imgName + '.png" width="20" height="20"> ';
+            valueOfInput += lastIndexAfterShortCut;
+         }
+
       });
       if (isShortCutFound) {
          isShortCutFound = false;
          _this.innerHTML = valueOfInput;
-         placeCaretAtEnd(_this);
+         placeCaret(_this);
+
       }
    };
-}(EMOGI = window.EMOGI = {}));
+} (EMOGI = window.EMOGI = {}));
